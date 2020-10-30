a0cd41724b1a284dabc66a8aae21a8c2a003ed28
周顺顺 <zhoushunshun@k12cloud.cn>
2020-10-30 11:40:12 +0800

    no message

--- work_system/src/main/java/com/idiotic/dao/SignUpDao.java
+++
@@ -1,9 +0,0 @@
-package com.idiotic.dao;
-
-import com.idiotic.domain.signUp.Happiness;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface SignUpDao extends JpaRepository<Happiness,String>, JpaSpecificationExecutor<Happiness> {
-    public Happiness findByMobile(String mobile);
-}
