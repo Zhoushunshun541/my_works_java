efd2b452e619baf4cb07e02848a27c6bdf21efa3
周顺顺 <zhoushunshun@k12cloud.cn>
2020-11-04 13:20:34 +0800

    no message

--- work_model/src/main/java/com/idiotic/domain/system/MyInfo.java
+++ work_model/src/main/java/com/idiotic/domain/system/MyInfo.java
@@ -9,77 +9,12 @@ import java.util.Objects;
 @Entity
 @Table(name = "my_info", schema = "my_works", catalog = "")
 public class MyInfo {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String skills;
     private String aboutMe;
     private String selfIntroduce;
     private Integer userId;
-
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    @Basic
-    @Column(name = "skills")
-    public String getSkills() {
-        return skills;
-    }
-
-    public void setSkills(String skills) {
-        this.skills = skills;
-    }
-
-    @Basic
-    @Column(name = "about_me")
-    public String getAboutMe() {
-        return aboutMe;
-    }
-
-    public void setAboutMe(String aboutMe) {
-        this.aboutMe = aboutMe;
-    }
-
-    @Basic
-    @Column(name = "self_introduce")
-    public String getSelfIntroduce() {
-        return selfIntroduce;
-    }
-
-    public void setSelfIntroduce(String selfIntroduce) {
-        this.selfIntroduce = selfIntroduce;
-    }
-
-    @Basic
-    @Column(name = "user_id")
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MyInfo myInfo = (MyInfo) o;
-        return id == myInfo.id &&
-                Objects.equals(skills, myInfo.skills) &&
-                Objects.equals(aboutMe, myInfo.aboutMe) &&
-                Objects.equals(selfIntroduce, myInfo.selfIntroduce) &&
-                Objects.equals(userId, myInfo.userId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, skills, aboutMe, selfIntroduce, userId);
-    }
 }
